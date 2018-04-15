0af5ab2c444e54caa0a32cbe451cdcc55f1cf7b7
Ali Abdollahi <abdollahiali@users.noreply.github.com>
2018-04-14 23:46:20 -0700

    print the number of arguments of a function without argument

--- 017_print_number_of_arguments.asm
+++ 017_print_number_of_arguments.asm
@@ -1,10 +1,13 @@
 %include "asm_nasm.inc"
+section .data
+	new_line db 10, 0
 
 
 section .text
 	global _start
 
 _start:
-	pop rax ; this pops the number of arguments of this program which is 1
+	pop rax ; rax holds 1 now, because the function has not argument
 	printint rax
+	printstr new_line
 	exit
