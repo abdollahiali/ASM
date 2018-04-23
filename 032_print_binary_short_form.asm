e96f3b852c9eec4627e497cb0343e3666834ed52
Ali Abdollahi <abdollahiali@users.noreply.github.com>
2018-04-23 01:44:10 -0700

    print binary representation (without zeros on the left) of an integer

---
+++ 032_print_binary_short_form.asm
@@ -0,0 +1,27 @@
+%include "asm_nasm.inc"
+
+section .data
+	str_rax db "rax = ", 0
+	str_bin db "Its binary representation is:", 10, 0
+
+section .text
+	global _start
+
+_start:
+	mov rax, 30
+	printstr str_rax
+	printint rax
+	printstr new_line
+	
+	printstr str_bin
+	printbin rax
+	printstr new_line
+	
+	exit
+
+	
+
+
+	
+	
+
