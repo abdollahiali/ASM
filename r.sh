7ccd037aae3b507b74fb32ffd366e571be0cb6cf
Ali Abdollahi <abdollahiali@users.noreply.github.com>
2018-05-11 22:34:58 -0700

    update the batch file to create the list file

--- r.sh
+++ r.sh
@@ -1,5 +1,8 @@
 clear
-nasm -f elf64 $1.asm -o $1.o
+# nasm -f elf64 $1.asm -o $1.o
+# previously I used the above one
+# to create the list file we can use the following command
+nasm -f elf64 $1.asm -l $1.lst
 
 ld $1.o -o $1
 
